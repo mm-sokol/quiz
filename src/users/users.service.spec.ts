97560bfeb15a4f95c2832ab5fbfefa621c7e202e
Małgorzata Sokół <msokol464@gmail.com>
2023-11-12 16:07:38 +0100

    Add create test for user service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUsersRepository = {
+    create: jest.fn().mockImplementation(dto => dto),
+    save: jest.fn().mockImplementation(user => Promise.resolve({id: Date.now(), ...user}))
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUsersRepository
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -15,4 +27,18 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+
+  it('should create a new user record and return that', async () => {
+    const userInput = {
+      username: 'rob_ot',
+      firstname: 'Robert',
+      lastname: 'Otto'
+    };
+    expect(await service.create(userInput)).toEqual({
+      id: expect.any(Number),
+      ...userInput
+    });
+  });
+
 });
