15787856f885e5d7c89fa7258a2f3b9b2f5f167e
Małgorzata Sokół <msokol464@gmail.com>
2023-11-17 11:09:34 +0100

    Add dataSource to quiz module

--- src/quizes/quizes.module.ts
+++ src/quizes/quizes.module.ts
@@ -4,9 +4,10 @@ import { QuizesResolver } from './quizes.resolver';
 import { Quiz } from './entities/quiz.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuestionsModule } from 'src/questions/questions.module';
+import { DataSourceProvider } from 'src/db/data-source.provider';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Quiz]), QuestionsModule],
-  providers: [QuizesResolver, QuizesService],
+  providers: [QuizesResolver, QuizesService, DataSourceProvider],
 })
 export class QuizesModule {}
