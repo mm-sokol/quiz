b962db63c993767322697228b4883b8bc23aca04
Małgorzata Sokół <msokol464@gmail.com>
2023-11-26 15:01:03 +0100

    Add fields to quiz-takes entity

--- src/quiz-takes/entities/quiz-take.entity.ts
+++ src/quiz-takes/entities/quiz-take.entity.ts
@@ -1,7 +1,35 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Quiz } from 'src/quizes/entities/quiz.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, CreateDateColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
 export class QuizTake {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  
+  @PrimaryGeneratedColumn()
+  @Field(() => ID)
+  id: number;
+
+  @Column()
+  @Field(() => Int)
+  quizId: number;
+
+  @Column()
+  @Field(() => Int)
+  userId: number;
+
+  @Column({nullable: true, default: 0})
+  @Field(() => Int, {nullable: true, defaultValue: 0})
+  score: number;
+
+  @CreateDateColumn()
+  @Field()
+  date: Date;
+
+  @ManyToOne(() => Quiz, (quiz) => quiz.takes)
+  quiz: Quiz;
+
+  @ManyToOne(() => User, (user) => user.quizTakes)
+  user: User;
+
 }
