7aa1ce3fd7f0d3570a74923c0ffc8278009bbdca
Małgorzata Sokół <msokol464@gmail.com>
2023-11-26 15:05:12 +0100

    Add quiz-take input classes

--- src/quiz-takes/dto/create-quiz-take.input.ts
+++ src/quiz-takes/dto/create-quiz-take.input.ts
@@ -1,7 +1,41 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { Int, Field, InputType, ID } from '@nestjs/graphql';
 
 @InputType()
 export class CreateQuizTakeInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+
+  @Field(() => Int)
+  quizId: number;
+
+  @Field(() => Int)
+  userId: number;
+
+  @Field(() => [QuestionInput])
+  givenAnswers: QuestionInput[];
+
+}
+
+@InputType()
+export class QuestionInput {
+
+  @Field(() => ID)
+  questionId: number;
+
+  @Field()
+  text?: string;
+
+  @Field(() => [ID])
+  correctAnswers?: number[];
+
+  @Field(() => [SortingInput])
+  sortedAnswers?: SortingInput[];
+}
+
+@InputType()
+export class SortingInput {
+  
+  @Field(() => Int)
+  answerId: number;
+
+  @Field(() => Int)
+  orderingNumber: number;
 }
--- src/quiz-takes/dto/update-quiz-take.input.ts
+++ src/quiz-takes/dto/update-quiz-take.input.ts
@@ -1,8 +1,8 @@
 import { CreateQuizTakeInput } from './create-quiz-take.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
-@InputType()
-export class UpdateQuizTakeInput extends PartialType(CreateQuizTakeInput) {
-  @Field(() => Int)
-  id: number;
-}
+// @InputType()
+// export class UpdateQuizTakeInput extends PartialType(CreateQuizTakeInput) {
+//   @Field(() => Int)
+//   id: number;
+// }
