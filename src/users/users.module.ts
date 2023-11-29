54d6ef4738f353090d0f2b111bdfcddcddaff7dc
Małgorzata Sokół <msokol464@gmail.com>
2023-11-29 15:31:11 +0100

    Add DataSourceProvider to user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { DataSourceProvider } from 'src/db/data-source.provider';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersResolver, UsersService],
+  providers: [UsersResolver, UsersService, DataSourceProvider],
 })
 export class UsersModule {}
